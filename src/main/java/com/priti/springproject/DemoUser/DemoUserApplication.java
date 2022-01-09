6170dfdc102017f9d06a092afb7837a256999401
priti kumari <151085priti@gmai;.com>
2022-01-09 22:07:56 +0800

    intial commit

---
+++ src/main/java/com/priti/springproject/DemoUser/DemoUserApplication.java
@@ -0,0 +1,13 @@
+package com.priti.springproject.DemoUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUserApplication.class, args);
+	}
+
+}
